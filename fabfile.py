47ce5bbbc61caaf2dbe57f0490d13a4f975ca9fe
Alex Clark <aclark@aclark.net>
2010-09-22 08:00:11 -0400

    Add comments

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-
+# A work in progress
 from fabric.api import env, put, run
 
 env.user = 'root'
@@ -10,6 +10,10 @@ def deploy():
 
 def update_packages():
     run('aptitude update')
+
+    # XXX What do I do when apitude asks "harder" questions? i.e. How
+    # does Fabric handle more complex interactions (with remote hosts) 
+    # a la (TCL) Expect? My understanding is that it currently does not.
     run('aptitude -y safe-upgrade')
 
 def copy_pub_key():
